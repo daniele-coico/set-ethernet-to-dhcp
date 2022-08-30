b97a0b9bf49daa9184af6be96de050ac1e32eb1f
Daniele Coico <51930487+daniele-coico@users.noreply.github.com>
2022-08-30 09:46:15 +0200

    Script upload
    First version

---
+++ SCRIPT AUTO-DHCP.ps1
@@ -0,0 +1,21 @@
+﻿$IPType = "IPv4"
+$adapter = Get-NetAdapter | ? {$_.Name -contains "Ethernet"}
+$interface = $adapter | Get-NetIPInterface -AddressFamily $IPType
+echo $interface
+
+$interface | ForEach-Object -Process {
+    If ($interface.Dhcp -eq "Disabled") 
+    {
+       # Remove existing gateway
+       If (($interface | Get-NetIPConfiguration).Ipv4DefaultGateway) {
+       $interface | Remove-NetRoute -Confirm:$false
+       echo "Imposto il gateaway predefinito"
+       }
+       # Enable DHCP
+       $interface | Set-NetIPInterface -DHCP Enabled
+        echo "Imposto il DHCP"
+       # Configure the DNS Servers automatically
+       $interface | Set-DnsClientServerAddress -ResetServerAddresses
+       echo "Resetto i DNS"
+    }
+}
